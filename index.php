460b48c28b6059257db0ad6c21e459509fd65381
deunizios <deunizios@gmail.com>
2015-11-14 21:58:52 -0200

    Topo do site #4

--- index.php
+++ index.php
@@ -3,10 +3,32 @@
     <head>
         <meta charset="UTF-8">
         <title>Site Institucional do SENAI - Brusque</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/estilo.css" />
+
+        <!-- Javascript -->
+        <script src="js/jquery-1.11.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/meuscript.js"></script>
+
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            <header class="jumbotron text-center" id="topo">
+                <h1>Bootstrap</h1>
+                <p>Alo ha Mundo do Bootstrap!</p>
+                <form role="form">
+                    <div class="form-inline">
+                        <div class="input-group input-group-sm">
+                            <span class="input-group-addon">
+                                <span class="glyphicon glyphicon-envelope"></span>
+                            </span>
+                            <input type="email" class="form-control" id="email" placeholder="seunome@seuprovedor.com.br"/>
+                        </div>
+                        <button type="submit" class="btn btn-success">Cadastrar</button>
+                    </div>
+                </form>
+            </header>
+        </div>
     </body>
 </html>
